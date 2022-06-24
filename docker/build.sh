262e5cf5b9307cdaf605fc98acbd6ef9ff63f48b
Alex Graber <alex.graber@pmi.org>
2022-06-24 12:16:28 -0400

    clean up build script

--- docker/build.sh
+++ docker/build.sh
@@ -4,19 +4,3 @@ docker buildx build \
   --tag="ninerealmlabs/mlflow-server:latest" \
   --load \
   .
-
-  # --progress=plain \
-  # --platform="linux/amd64" \
-
-docker run \
-  --rm \
-  -p 5555:5555 \
-  --env
-  ninerealmlabs/mlflow-server:latest
-
-
-docker run \
-  --rm \
-  -it \
-  -p 5555:5000 \
-  ninerealmlabs/mlflow-server:latest sh
