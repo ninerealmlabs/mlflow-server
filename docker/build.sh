c1ea9bd81c862ac4acd0292fb172bc864c2fc5ac
ahgraber <ahgraber@ninerealmlabs.com>
2025-05-26 13:16:59 -0400

    chore: update copier configuration and merge configuration files
    - Added new configuration files: .copier-answers.yaml, .mdformat.toml, and .markdownlint.yaml.
    - Updated .editorconfig for consistent formatting rules.
    - Enhanced .pre-commit-config.yaml with additional hooks and improved configurations.
    - Modified .gitignore to include more ignored files and directories.
    - Updated Dockerfile to use VCS_REF instead of GIT_COMMIT for better version control.
    - Cleaned up .shellcheckrc and .typos.toml for improved linting and typo checking.
    - Adjusted docker-compose.yaml for better environment variable handling.

--- docker/build.sh
+++ docker/build.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 docker buildx create --use --name builder;
 docker buildx build \
-  --tag="ninerealmlabs/mlflow-server:latest" \
-  --load \
-  .
+    --tag="ninerealmlabs/mlflow-server:latest" \
+    --load \
+    .
